edb7399202aa48b5ac18ab536201c12b9bb40a39
jdbriner07 <jdbriner07@gmail.com>
2017-09-25 14:31:48 -0700

    styled some

--- client/components/search.js
+++ client/components/search.js
@@ -3,27 +3,31 @@ angular.module('dogCatcher')
 	$scope.ctrl.dogToBeCaught = '';
 	$scope.ctrl.errorMessage = '';
 	$scope.ctrl.catchDog = function() {
-		let dog = $scope.ctrl.dogToBeCaught.split('-'), url;
-		if (dog.length === 1) {
-			url = `https://dog.ceo/api/breed/${dog[0]}/images/random`;
+		if (!$scope.ctrl.dogToBeCaught) {
+			$scope.ctrl.errorMessage = 'Please specify a dog';
 		} else {
-			url = `https://dog.ceo/api/breed/${dog[0]}/${dog[1]}/images/random`;
-		}
-		$http({
-			method: 'GET',
-			url: url
-		}).then(function(response) {
-			if (response.data.status === 'success') {
-				let breed = response.data.message.split('/')[5];
-				$scope.ctrl.dogsCaught.push({breed: breed, picture: response.data.message});
-				$scope.ctrl.errorMessage = '';
+			let dog = $scope.ctrl.dogToBeCaught.split('-'), url;
+			if (dog.length === 1) {
+				url = `https://dog.ceo/api/breed/${dog[0]}/images/random`;
 			} else {
-				$scope.ctrl.errorMessage = 'That breed doesn\'t exist!';
+				url = `https://dog.ceo/api/breed/${dog[0]}/${dog[1]}/images/random`;
 			}
-		}, function(response) {
-			console.log(response);
-		})
-		$scope.ctrl.dogToBeCaught = '';
+			$http({
+				method: 'GET',
+				url: url
+			}).then(function(response) {
+				if (response.data.status === 'success') {
+					let breed = response.data.message.split('/')[5];
+					$scope.ctrl.dogsCaught.push({breed: breed, picture: response.data.message});
+					$scope.ctrl.errorMessage = '';
+				} else {
+					$scope.ctrl.errorMessage = 'That breed doesn\'t exist!';
+				}
+			}, function(response) {
+				console.log(response);
+			})
+			$scope.ctrl.dogToBeCaught = '';
+		}
 	}
 })
 .directive('search', function() {
